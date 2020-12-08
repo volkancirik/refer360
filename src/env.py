lookahead fixes

src/env.py
@@ -7,8 +7,8 @@ from utils import load_datasets
 from model_utils import smoothed_gaussian
 from model_utils import gaussian_target
 from utils import rad2degree
-# from panoramic_camera import PanoramicCamera as camera
-from panoramic_camera_gpu import PanoramicCameraGPU as camera
+from panoramic_camera import PanoramicCamera as cpu_camera
+from panoramic_camera_gpu import PanoramicCameraGPU as gpu_camera
 from torchvision import transforms
 import numpy as np
 import torch
@@ -26,7 +26,8 @@ class EnvBatch():
                image_h=FOV_SIZE,
                image_w=FOV_SIZE,
                fov=90,
-               degrees=5):
+               degrees=5,
+               use_gpu_camera=False):
     '''Initialize camerase for a new batch of environment.
     '''
 
@@ -37,6 +38,13 @@ class EnvBatch():
                      4: (-self.degrees, 0),
                      }
 
+    if use_gpu_camera:
+      camera = gpu_camera
+      print('gpu camera will be used')
+    else:
+      print('cpu camera will be used')
+      camera = cpu_camera
+
     self.batch_size = batch_size
     self.image_w = image_w
     self.image_h = image_h
@@ -45,15 +53,9 @@ class EnvBatch():
     shape = (image_h, image_w)
     # set # of cameras batch_size
     self.cameras = [camera(fov=fov,
-                           output_image_shape=shape,
-                           use_tensor_image=False) for ii in range(self.batch_size)]
+                           output_image_shape=shape) for ii in range(self.batch_size)]
     # keep track of which cameras are done
     self._done = [False for ii in range(self.batch_size)]
-    self.preprocess = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-    ])
 
   def newEpisodes(self, pano_paths, gt_loc=[]):
     '''Initialize cameras with new images.
@@ -116,9 +118,10 @@ class EnvBatch():
     for ii in range(self.batch_size):
       orig_lat = self.cameras[ii].lat
       orig_lng = self.cameras[ii].lng
-      neighbors = [np.zeros((FOV_SIZE, FOV_SIZE, 3), dtype='uint8')]*4
+      neighbors = [np.zeros((FOV_SIZE, FOV_SIZE, 3), dtype='uint8')]*5
       if not self._done[ii]:
-        neighbors = []
+        img = self.cameras[ii].get_image()
+        neighbors = [img]
         for navigate in [1, 2, 3, 4]:
           longitude = orig_lng + self.__look__[navigate][1]
           latitude = orig_lat + self.__look__[navigate][0]
@@ -141,13 +144,7 @@ class EnvBatch():
           neighbors.append(img)
         self.cameras[ii].look(orig_lat, orig_lng)
 
-      im_stack = []
-      for n in neighbors:
-        pil_img = Image.fromarray(n)
-        img_tensor = self.preprocess(pil_img)
-        im_stack.append(img_tensor.unsqueeze(0))
-
-      self.cameras[ii].neighbors = torch.cat(im_stack, 0).cuda()
+      self.cameras[ii].neighbors = neighbors
 
 
 class Refer360Batch():
@@ -162,6 +159,7 @@ class Refer360Batch():
                use_look_ahead=False,
                use_sentences=False,
                use_gt_action=False,
+               use_gpu_camera=False,
                oracle_mode=False,
                prepare_vocab=False,
                images='all'):
@@ -172,6 +170,7 @@ class Refer360Batch():
     self.fov = 90
     self.degrees = degrees
     self.use_look_ahead = use_look_ahead
+    self.use_gpu_camera = use_gpu_camera
 
     # observed image size
     self.observation_space = (3, FOV_SIZE, FOV_SIZE)
@@ -183,7 +182,8 @@ class Refer360Batch():
                         image_h=self.image_h,
                         image_w=self.image_w,
                         fov=self.fov,
-                        degrees=self.degrees)
+                        degrees=self.degrees,
+                        use_gpu_camera=self.use_gpu_camera)
     # keep a list of datum
     self.data = []
 
@@ -222,8 +222,6 @@ class Refer360Batch():
     self.coor_threshold = 20
     self.preprocess = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
     ])
     self.predictions = [None for ii in range(self.batch_size)]
 
@@ -300,7 +298,7 @@ class Refer360Batch():
     '''Returns a list of information and meta-data
     '''
     # keep a list of metadata
-    infos = []
+    observations = []
     # keep a list of image observations
     im_stack = []
 
@@ -312,6 +310,7 @@ class Refer360Batch():
 
       # Check dimension order
       perspective_image = state[0]
+      pixel_map = state[1]
 
       pil_img = Image.fromarray(perspective_image)
       img_tensor = self.preprocess(pil_img)
@@ -327,7 +326,7 @@ class Refer360Batch():
       else:
         gt_tuple, gt_coor = (None, None, None), None
 
-      infos.append({
+      observations.append({
           'id': datum['annotationid'],
           'gt_moves': datum['gt_moves'][0],
           'refexps': refexps,
@@ -337,14 +336,14 @@ class Refer360Batch():
           'gt_tuple': gt_tuple,
           'gt_coor': gt_coor,
           'pano': datum['pano'],
-          'img': state[0],
-          'pixel_map': state[1],
+          'img': perspective_image,
+          'pixel_map': pixel_map,
           'latitude': self.env.cameras[ii].lat,
           'longitude': self.env.cameras[ii].lng,
           'neighbors': self.env.cameras[ii].neighbors,
       })
     im_batch = torch.cat(im_stack, 0).cuda()
-    return im_batch, infos
+    return im_batch, observations
 
   def reset_panos(self):
     '''Random start lat/lng
@@ -460,14 +459,14 @@ class Refer360Batch():
                                         height=100, width=100)
             kl_loss = kl_loss.item()
             reward[ii] = (-kl_loss)*10
-            self.env._done[ii] = True
             self.predictions[ii] = (argmax_pred, (gt_x, gt_y))
+            self.env._done[ii] = True
           else:
-            # give 0 reward since waldo in fov
-            reward[ii] = 0.
+            # give negative reward since waldo in fov
+            reward[ii] = -10.
         elif navigate == 0:
           # waldo is not in fov
-          reward[ii] = life_penalty*2
+          reward[ii] = -10.  # *life_penalty*2
           self.env._done[ii] = True
         elif navigate == 5 and self.use_sentences:
           n_sentence = self.sentence_ids[ii]

src/models/finder_look_ahead.py
@@ -57,7 +57,13 @@ class FinderLookAhead(nn.Module):
     self.n_layers = args.n_layers
 
     self.cnn = nn.Sequential(
-        *(list(models.resnet18(pretrained=True).children())[:-args.cnn_layer])).cuda().eval()
+        *(list(models.resnet18(pretrained=True).children())[:5])).cuda().eval()
+    # resnetmodel = models.resnet152(pretrained=True).cuda().eval()
+    # newmodel = torch.nn.Sequential(
+    #     *(list(resnetmodel.children())[:-6])).cuda()
+    # for p in newmodel.parameters():
+    #   newmodel.requires_grad = False
+    # self.cnn = newmodel
 
     self.localizer = get_localizer(args, self.n_vocab)
 
@@ -88,51 +94,38 @@ class FinderLookAhead(nn.Module):
                    for o in observations['observations']]
 
     texts, texts_mask, texts_emb_mask, seq_lengths = vectorize_seq(
-        instruction, self.vocab, self.n_emb, cuda=True, permute=self.use_masks)
+        instruction, self.vocab, 64, cuda=True, permute=self.use_masks)
 
-    # return text_embed with localizer?
     kwargs = {}
     if self.use_masks:
       kwargs['texts_mask'] = texts_mask
       kwargs['texts_emb_mask'] = texts_emb_mask
 
-    if self.use_raw_image:
-      kwargs['im_batch'] = observations['im_batch']
-      kwargs['imgs'] = [o['img']
-                        for o in observations['observations']]
-      kwargs['instruction'] = instruction
-      loc_pred = self.localizer(kwargs)
-    else:
-      images = self.cnn(observations['im_batch'])
-      loc_pred = self.localizer(images, texts, seq_lengths.cpu(), **kwargs)
-
-    obs = loc_pred.reshape(
-        self.batch_size, loc_pred.size(1) * loc_pred.size(2))
+    if not self.use_raw_image:
+      raise NotImplementedError()
 
     neighbors = [o['neighbors']
                  for o in observations['observations']]
-    all_neighbors = torch.cat(neighbors, 0)
-    neighbors_feat = self.cnn(all_neighbors)
-    neighbors_flat = neighbors_feat.reshape(
-        self.batch_size * 4, 64, loc_pred.size(1) * loc_pred.size(2)).permute(0, 2, 1)
 
-    neighbors_pred = self.neighbor2pred(neighbors_flat).squeeze()
-    neighbors_stack = neighbors_pred.reshape(self.batch_size, 4, 10000)
-    obs_stack = obs.unsqueeze(1).repeat(1, 4, 1)
+    neighbor_scores = []
+    for n in range(5):
+      neighbor_imgs = []
+      for bb, neighbor in enumerate(neighbors):
+        neighbor_imgs.append(neighbor[n])
 
-    fused = neighbors_stack * obs_stack
-    neighbors_scores = self.act2out(fused).squeeze(2)
-    pred_score = self.loc2pred(obs)
+      kwargs['imgs'] = neighbor_imgs
+      kwargs['instruction'] = instruction
+      loc_pred = self.localizer(kwargs)
+      neighbor_pred = loc_pred.reshape(
+          self.batch_size, loc_pred.size(1) * loc_pred.size(2))
 
-    action_logits = torch.cat([pred_score, neighbors_scores], 1)
+      if n == 0:
+        loc_pred = loc_pred
+      pred_score = self.loc2pred(neighbor_pred)
+      neighbor_scores.append(pred_score)
+
+    action_logits = torch.cat(neighbor_scores, 1)
     state_values = self.val2out(action_logits)
-
-    # state = self.obs2state(obs)
-    # state_now, rnn_hidden = self.memory(
-    #     state.unsqueeze(0), rnn_hidden)
-    # state_now = state_now.squeeze(0)
-
-    # action_logits,a state_values = self.actor(state_now, neighbors)
 
     out = {}
     out['action_logits'] = action_logits

src/models/lxmert.py
@@ -9,9 +9,9 @@ from lxrt.modeling import BertLayerNorm, GeLU
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
-
+from torchvision import transforms
 import numpy as np
-
+from PIL import Image
 # Max length including <bos> and <eos>
 MAX_SEQ_LENGTH = 100
 DATA_PATH = '../py_bottom_up_attention/demo/data/genome/1600-400-20'
@@ -106,7 +106,7 @@ def extract_detectron2_features(detector, raw_images):
       for nms_thresh in np.arange(0.3, 1.0, 0.1):
         instances, ids = fast_rcnn_inference_single_image(
             boxes, probs, image_size,
-            score_thresh=0.2, nms_thresh=nms_thresh, topk_per_image=MAX_BOXES
+            score_thresh=0.5, nms_thresh=nms_thresh, topk_per_image=MAX_BOXES
         )
         if len(ids) >= MIN_BOXES:
           break
@@ -134,7 +134,9 @@ def extract_detectron2_features(detector, raw_images):
 
 
 class LXMERTLocalizer(nn.Module):
-  def __init__(self, args, num_logits=100*100):
+  def __init__(self, args,
+               width=100,
+               height=100):
     super().__init__()
 
     # Build LXRT encoder
@@ -143,6 +145,9 @@ class LXMERTLocalizer(nn.Module):
         max_seq_length=MAX_SEQ_LENGTH
     )
     hid_dim = self.lxrt_encoder.dim
+    num_logits = width * height
+    self.width = width
+    self.height = height
     self.n_actions = num_logits
 
     self.logit_fc = nn.Sequential(
@@ -180,6 +185,11 @@ class LXMERTLocalizer(nn.Module):
       self.cnn = nn.Sequential(
           *(list(models.resnet18(pretrained=True).children())[:-3])).cuda().eval()
       self.cnn2box = nn.Linear(256, 2048)
+      self.preprocess = transforms.Compose([
+          transforms.ToTensor(),
+          transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                               std=[0.229, 0.224, 0.225]),
+      ])
 
   def get_det2_boxes(self, imgs):
 
@@ -221,10 +231,16 @@ class LXMERTLocalizer(nn.Module):
     if self.use_detectron:
       feats, boxes = self.get_det2_boxes(inp['imgs'])
     else:
-      feats, boxes = self.get_cnn_boxes(inp['im_batch'])
+      imgs = []
+      for img in inp['imgs']:
+        pil_img = Image.fromarray(img)
+        i = self.preprocess(pil_img).unsqueeze(0).cuda()
+        imgs.append(i)
+      img_tensor = torch.cat(imgs, 0)
+      feats, boxes = self.get_cnn_boxes(img_tensor)
 
     x = self.lxrt_encoder(instruction, (feats, boxes))
     batch_size = x.size(0)
-    logit = self.logit_fc(x).view(batch_size, 100, 100)
+    logit = self.logit_fc(x).view(batch_size, self.height, self.width)
 
     return logit

src/train_rl.py
@@ -2,13 +2,20 @@
 RL agent playing the game
 """
 import paths
+import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+# os.environ['GPU_DEBUG'] = '2'
+
+import sys
+from gpu_profile import gpu_profile
+
 from env import Refer360Batch
 from get_model import get_model
 from arguments import get_train_rl
 import numpy as np
 from collections import namedtuple
 import json
-import os
+
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -16,11 +23,11 @@ from torch.distributions import Categorical
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from collections import defaultdict
-
+import gc
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 eps = np.finfo(np.float32).eps.item()
 torch.backends.cudnn.benchmark = True
-torch.set_default_tensor_type('torch.cuda.FloatTensor')
+# torch.set_default_tensor_type('torch.cuda.FloatTensor')
 #device = torch.device('cuda')
 
 
@@ -125,6 +132,9 @@ def finish_episode(model, optimizer, clip, update=False):
 
   policy_loss = 0.0
   value_loss = 0.0
+
+  pls = []
+  vls = []
   for bb in range(model.batch_size):
 
     rewards = model.rewards[bb]
@@ -134,7 +144,12 @@ def finish_episode(model, optimizer, clip, update=False):
     policy_loss += pl
     value_loss += vl
 
+    pls.append(pl.item())
+    vls.append(vl.item())
+
   loss = policy_loss + value_loss
+
+  gpu_profile(frame=sys._getframe(), event='line', arg=None)
   if update:
     optimizer.zero_grad()
     loss.backward()
@@ -143,6 +158,7 @@ def finish_episode(model, optimizer, clip, update=False):
 
   del model.rewards
   del model.saved_actions
+
   model.rewards = [[] for bb in range(model.batch_size)]
   model.saved_actions = [[] for bb in range(model.batch_size)]
 
@@ -178,16 +194,17 @@ def eval_epoch(ref360env, model, optimizer, args,
   else:
     pbar = range(n_updates)
 
+  rnn_hidden = torch.zeros(args.n_layers,
+                           batch_size, args.n_hid).cuda()
+
   for bid in pbar:
     done_list = [False]*batch_size
 
-    batch_metrics = defaultdict(list)
-    for metric in ['reward']:
-      batch_metrics[metric] = [list() for kk in range(batch_size)]
-    done_set = set()
+    batch_metrics = {}
+    batch_metrics['reward'] = [list() for kk in range(batch_size)]
+    batch_metrics['step'] = [list() for kk in range(batch_size)]
 
-    rnn_hidden = torch.zeros(args.n_layers,
-                             batch_size, args.n_hid).cuda()
+    done_set = set()
 
     history = {}
     for o in observations:
@@ -248,6 +265,7 @@ def eval_epoch(ref360env, model, optimizer, args,
           batch_metrics['reward'][kk].append(r.item())
           if d:
             done_set.add(kk)
+            batch_metrics['step'][kk].append(_step+1)
 
     for kk in range(batch_size):
       if kk not in done_set:
@@ -280,6 +298,8 @@ def eval_epoch(ref360env, model, optimizer, args,
     logs = dict()
     log_list = [log_loss]
     for metric in sorted(epoch_mean):
+      if metric not in set(['loss', 'acc_40', 'fov', 'reward', 'step']):
+        continue
       logs[metric] = '{} E: {:3.3f} B: {:3.3f}'.format(metric,
                                                        epoch_mean[metric],
                                                        epoch_metrics[metric][-1])
@@ -334,6 +354,7 @@ def main():
                                 seed=args.seed,
                                 degrees=args.degrees,
                                 use_look_ahead=args.use_look_ahead,
+                                use_gpu_camera=args.use_gpu_camera,
                                 oracle_mode=args.oracle_mode,
                                 images=args.trn_images,
                                 prepare_vocab=True)
@@ -342,10 +363,13 @@ def main():
                                 seed=args.seed,
                                 degrees=args.degrees,
                                 use_look_ahead=args.use_look_ahead,
+                                use_gpu_camera=args.use_gpu_camera,
                                 oracle_mode=args.oracle_mode,
                                 images=args.val_images)
 
   model = get_model(args, trn_ref360env.vocab, n_actions=5)
+  if args.multi_gpu:
+    model = torch.nn.DataParallel(model)
   optimizer = optim.Adam(model.parameters(),
                          lr=args.lr,
                          weight_decay=args.weight_decay)
@@ -388,4 +412,5 @@ def main():
 
 
 if __name__ == '__main__':
+  sys.settrace(gpu_profile)
   main()